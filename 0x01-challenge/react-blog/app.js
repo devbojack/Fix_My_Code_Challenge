8782033658ec0381bf327eac03788837c0baf5c5
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-10-14 19:27:43 -0700

    react blog fix

--- 0x01-challenge/react-blog/app.js
+++ 0x01-challenge/react-blog/app.js
@@ -1,75 +1,60 @@
-/**
- * Express configurations.
- *
- * @type {exports|module.exports}
- */
-var Router = require('react-router');
-var React = require('react/addons');
-var express = require('express');
-var Iso = require('iso');
-var session = require('express-session');
-var path = require('path');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const React = require('react/addons');
+const Iso = require('iso');
+const Router = require('react-router');
+const alt = require('./src/alt');
+const config = require('./config.js');
+const posts = require('./routes/post.routes');
+const routes = require('./src/routes.jsx');
 
-//Define Routes here
-var posts = require('./routes/post.routes');
-var routes = require('./src/routes.jsx');
-var alt = require('./src/alt');
-var config = require('./config.js');
-var app = express();
+const app = express();
 
-app.set('views', __dirname+'/views');
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'copy cat', resave: false, saveUninitialized: true}));
+// Static files and middleware
+app.use(express.static(path.join(__dirname, 'public'));
+app.use(session({ secret: 'copy cat', resave: false, saveUninitialized: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({limit : '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
 
-//use Routes here
-app.use('/',posts);
+// Routes setup
+app.use('/', posts);
 
 app.use(function (req, res) {
+  const data = res.locals.data || {};
+  alt.bootstrap(JSON.stringify(data));
+  const metaDescription = res.locals.metaDescription || '';
+  const iso = new Iso();
 
-    var data = res.locals.data || {};
-    alt.bootstrap(JSON.stringify(data));
-    
-    var metaDescription = res.locals.metaDescription || '';
+  Router.run(routes, req.url, function (Handler) {
+    const content = React.renderToString(React.createElement(Handler));
+    iso.add(content, alt.flush());
 
-    var iso = new Iso();
-
-    Router.run(routes, req.url, function (Handler) {
-
-        var content = React.renderToString(React.createElement(Handler));
-
-        iso.add(content, alt.flush());
-        
-        res.render('index',{
-            content:iso.render(),
-            pageTitle: config.pageTitle,
-            metaDescription: metaDescription
-        });
+    res.render('index', {
+      content: iso.render(),
+      pageTitle: config.pageTitle,
+      metaDescription: metaDescription
     });
+  });
 });
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-
-    if(!err.status || err.status !== 404){
-        err.status = 500;
-    }
-
-    console.log(err);
-
-    res.status(err.status);
-
-    res.sendFile(path.resolve(__dirname+'/views/error/'+err.status+'.html'));
-
+// Production error handler
+app.use(function (err, req, res, next) {
+  if (!err.status || err.status !== 404) {
+    err.status = 500;
+  }
+  console.error(err);
+  res.status(err.status);
+  res.sendFile(path.resolve(__dirname + '/views/error/' + err.status + '.html');
 });
 
 app.listen(config.port, function () {
-    console.log('Listening on ' + config.baseUrl);
+  console.log('Listening on ' + config.baseUrl);
 });
